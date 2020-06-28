158dd743b5fba05ba1d4696943b7c65e46e09737
SEOzizou <44471240+SEOzizou@users.noreply.github.com>
2020-06-28 20:48:44 +0900

    Update st.sh
    remark add

--- st.sh
+++ st.sh
@@ -6,9 +6,9 @@ pip3 install bs4
 pip3 install numpy
 
 
-mkdir -p project/Webcrawling
-cp -r app_final.py templates_final project/Webcrawling
-mv project/Webcrawling/templates_final project/Webcrawling/templates
+mkdir -p project/Webcrawling # directory 생성
+cp -r app_final.py templates_final project/Webcrawling # project/Webcrawling에 최종 파일 복사 
+mv project/Webcrawling/templates_final project/Webcrawling/templates #templates_final templates로 이름 바꾼다.
 echo copy source files and templates
 
 gnome-terminal -x ./elasticsearch-7.6.2/bin/elasticsearch
